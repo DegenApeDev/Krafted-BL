a748b9874325c73f5abe2aaf105f48621745bb83
linuzo <linuzo@kidzzone.ca>
2017-01-01 12:10:20 -0700

    update readme

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -75,7 +75,7 @@ namespace pocketmine {
 
 	const VERSION = "1.9dev";
 	const API_VERSION = "2.0.0";
-	const CODENAME = "BlueLight";
+	const CODENAME = "BL-Kidz";
 	const MINECRAFT_VERSION = "v1.0.0 alpha";
 	const MINECRAFT_VERSION_NETWORK = "1.0.0";
 	const BLUELIGHT_VERSION = "2.0.0";
